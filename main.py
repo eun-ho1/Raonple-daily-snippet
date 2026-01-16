70c95b809ef0778de3e1c85a380851a123287fa6
Eunho Jung <jeh0224@gachon.ac.kr>
2026-01-17 03:48:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ TEAM_INFO = {
 
 def get_target_date_kst():
     # 현재 테스트를 위해 오늘 날짜 기준 (어제 데이터를 보내려면 - timedelta(days=1) 추가)
-    return (datetime.utcnow() + timedelta(hours=9)).strftime("%Y-%m-%d")
+    return (datetime.utcnow() + timedelta(hours=9)).strftime("%Y-%m-%d")  - timedelta(days=1)
 
 def get_page_body_content(page_id):
     """노션 본문을 읽어 HTML 줄바꿈 형식으로 변환합니다."""
